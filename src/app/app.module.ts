7f67d134392e962784e7d4e934224545c0ad6bce
diana3664 <dianamus2017@gmail.com>
2021-05-14 18:33:12 +0300

    add  componet called quote-delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './components/quote/quote.component';
+import { QuoteDeleteComponent } from './components/quote-delete/quote-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteDeleteComponent
   ],
   imports: [
     BrowserModule,
