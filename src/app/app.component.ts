40ba9760206ec39c3c9d29ed752c24eab1c96638
diana3664 <dianamus2017@gmail.com>
2021-05-14 17:25:17 +0300

    import the class to our app.componet.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,10 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: string[];
-  constructor(){
-    this.quotes = ["On staying flexible. Don't make plans, make options.","I think the more unexpected something is, the more there is to learn from it.","One ought every day at least, to hear a little song, read a good poem, see a fine picture, and, if it were possible, to speak a few reasonable words."]
+  quotes :Quote [] = [
+    {id:1, quote:"On staying flexible. Don't make plans, make options."},
+    {id:2, quote:"I think the more unexpected something is, the more there is to learn from it."},
+    {id:2, quote:"One ought every day at least, to hear a little song, read a good poem, see a fine picture, and, if it were possible, to speak a few reasonable words."},
 
-  }
+  ]; 
 }
