cf42bd283e2bb4a79ba0713a80bc102fcd8d3569
diana3664 <dianamus2017@gmail.com>
2021-05-14 17:05:02 +0300

    add initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes: string[];
+  constructor(){
+    this.quotes = ["On staying flexible. Don't make plans, make options.","I think the more unexpected something is, the more there is to learn from it.","One ought every day at least, to hear a little song, read a good poem, see a fine picture, and, if it were possible, to speak a few reasonable words."]
+
+  }
 }
