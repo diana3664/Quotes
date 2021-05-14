c37237b0a0d4158c87d017b0ccec6304fe86cc6f
diana3664 <dianamus2017@gmail.com>
2021-05-14 18:20:26 +0300

    create the property goals and specified that it will be an array of type Goal

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -10,9 +10,9 @@ import { Quote } from 'src/app/quote';
 export class QuoteComponent implements OnInit {
 
   quotes :Quote [] = [
-    {id:1, quote:"On staying flexible. Don't make plans, make options."},
-    {id:2, quote:"I think the more unexpected something is, the more there is to learn from it."},
-    {id:2, quote:"One ought every day at least, to hear a little song, read a good poem, see a fine picture, and, if it were possible, to speak a few reasonable words."},
+    new Quote(1,"On staying flexible. Don't make plans, make options."),
+    new Quote(2,"I think the more unexpected something is, the more there is to learn from it."),
+    new Quote(2,"One ought every day at least, to hear a little song, read a good poem, see a fine picture, and, if it were possible, to speak a few reasonable words."),
 
   ]; 
   constructor() { }
