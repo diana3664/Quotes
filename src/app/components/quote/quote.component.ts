62453e4810d253f24a74112f893ffdb06f86f884
diana3664 <dianamus2017@gmail.com>
2021-05-16 15:04:43 +0300

    style our quote

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -11,9 +11,9 @@ export class QuoteComponent implements OnInit {
 
 
   quotes :Quote [] = [
-    new Quote("Dian K.","On staying flexible. Don't make plans, make options.","Nicholas N.",0,0),
-    new Quote("Christine C.","I think the more unexpected something is, the more there is to learn from it.", "Christoph H.",0,0),
-    new Quote("Martin L","One ought every day at least, to hear a little song, read a good poem, see a fine picture, and, if it were possible, to speak a few reasonable words.","Ann QQ",0,0),
+    // new Quote("Dian K.","On staying flexible. Don't make plans, make options.","Nicholas N.",0,0),
+    new Quote("Christine C.","I think the more unexpected something is, the more there is to learn from it, like this project.", "Christoph H.",0,0),
+    // new Quote("Martin L","One ought every day at least, to hear a little song, read a good poem, see a fine picture, and, if it were possible, to speak a few reasonable words.","Ann QQ",0,0),
 
   ]; 
 
